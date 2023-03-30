973d50e1492749f97c4af63606d32cf19e35310f
bethany <bethanylynnpro@gmail.com>
2023-03-30 12:18:30 -0700

    added another file

--- server.py
+++ server.py
@@ -1,6 +1,83 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify, request
 
 app = Flask(__name__)
+
+CARD_DATA = [
+    {
+        "name": "Abigail",
+        "birthday": "Fall 13",
+        "imgUrl": "/static/img/Abigail.png",
+        "cardId": 1,
+    },
+    {
+        "name": "Alex",
+        "birthday": "Spring 20",
+        "imgUrl": "/static/img/Alex.png",
+        "cardId": 2,
+    },
+    {
+        "name": "Elliot",
+        "birthday": "Fall 05",
+        "imgUrl": "/static/img/Elliot.png",
+        "cardId": 3,
+    },
+    {
+        "name": "Emily",
+        "birthday": "Spring 27",
+        "imgUrl": "/static/img/Emily.png",
+        "cardId": 4,
+    },
+    {
+        "name": "Haley",
+        "birthday": "Spring 14",
+        "imgUrl": "/static/img/Haley.png",
+        "cardId": 5,
+    },
+    {
+        "name": "Harvey",
+        "birthday": "Winter 14",
+        "imgUrl": "/static/img/Harvey.png",
+        "cardId": 6,
+    },
+    {
+        "name": "Leah",
+        "birthday": "Winter 23",
+        "imgUrl": "/static/img/Leah.png",
+        "cardId": 7,
+    },
+    {
+        "name": "Maru",
+        "birthday": "Summer 10",
+        "imgUrl": "/static/img/Maru.png",
+       "cardId": 8,
+    },
+    {
+        "name": "Penny",
+        "birthday": "Fall 02",
+        "imgUrl": "/static/img/Penny.png",
+        "cardId": 8,
+    },
+    {
+        "name": "Sam",
+        "birthday": "Summer 17",
+        "imgUrl": "/static/img/Sam.png",
+        "cardId": 9,
+    },
+    {
+        "name": "Sebastian",
+        "birthday": "Winter 10",
+        "imgUrl": "/static/img/Sebastian.png",
+        "cardId": 10,
+    },
+    {
+        "name": "Shane",
+        "birthday": "Spring 20",
+        "imgUrl": "/static/img/Shane.png",
+        "cardId": 11,
+    },
+
+]
+
 
 @app.route('/')
 def show_homepage():
@@ -13,6 +90,31 @@ def show_cards():
     """Show all players cards"""
 
     return render_template('cards.html')
+
+@app.route("/cards,json")
+def get_cards_json():
+    """Return JSON response with cards"""
+
+    return jsonify({"cards": CARD_DATA})
+
+@app.route("/add-card", methods=["POST"])
+def add_card():
+    """Add new card to DB"""
+    name = request.get_json().get("name")
+    birthday = request.get_json().get("birthday")
+
+    new_card = {
+        "name": name,
+        "birthday": birthday,
+        "imgUrl": "/static/img/placeholder.png",
+        "cardId": len(CARD_DATA) + 1,
+    }
+    CARD_DATA.append(new_card)
+    return jsonify({"success": True, "cardAdded": new_card})
+
+@app.route("/cards-no-react")
+def show_cards_no_react():
+    return render_template("cards-no-react.html")
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
--- static/js/charactercards.jsx
+++ static/js/charactercards.jsx
@@ -1,81 +1,3 @@
-'use strict';
-
-const characterCardData = [
-    {
-        name: "Abigail",
-        birthday: "Fall 13",
-        imgUrl: "/static/img/Abigail.png",
-        cardId: 1,
-    },
-    {
-        name: "Alex",
-        birthday: "Spring 20",
-        imgUrl: "/static/img/Alex.png",
-        cardId: 2,
-    },
-    {
-        name: "Elliot",
-        birthday: "Fall 05",
-        imgUrl: "/static/img/Elliot.png",
-        cardId: 3,
-    },
-    {
-        name: "Emily",
-        birthday: "Spring 27",
-        imgUrl: "/static/img/Emily.png",
-        cardId: 4,
-    },
-    {
-        name: "Haley",
-        birthday: "Spring 14",
-        imgUrl: "/static/img/Haley.png",
-        cardId: 5,
-    },
-    {
-        name: "Harvey",
-        birthday: "Winter 14",
-        imgUrl: "/static/img/Harvey.png",
-        cardId: 6,
-    },
-    {
-        name: "Leah",
-        birthday: "Winter 23",
-        imgUrl: "/static/img/Leah.png",
-        cardId: 7,
-    },
-    {
-        name: "Maru",
-        birthday: "Summer 10",
-        imgUrl: "/static/img/Maru.png",
-        cardId: 8,
-    },
-    {
-        name: "Penny",
-        birthday: "Fall 02",
-        imgUrl: "/static/img/Penny.png",
-        cardId: 8,
-    },
-    {
-        name: "Sam",
-        birthday: "Summer 17",
-        imgUrl: "/static/img/Sam.png",
-        cardId: 9,
-    },
-    {
-        name: "Sebastian",
-        birthday: "Winter 10",
-        imgUrl: "/static/img/Sebastian.png",
-        cardId: 10,
-    },
-    {
-        name: "Shane",
-        birthday: "Spring 20",
-        imgUrl: "/static/img/Shane.png",
-        cardId: 11,
-    },
-
-];
-
 function CharacterCard(props) {
     return (
         <div className="card">
@@ -87,12 +9,66 @@ function CharacterCard(props) {
 
 }
 
-console.log(characterCardData);
-ReactDOM.render(
-    <CharacterCard name="Abigail" birthday="Fall 13"  imgUrl="/static/img/Abigail.png"/>,
-    document.querySelector('#Abigail'),
-);
-ReactDOM.render(
-    <CharacterCard name="Alex" birthday="Spring 20"  imgUrl="/static/img/Alex.png"/>,
-    document.querySelector('#Alex'),
-);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// console.log(characterCardData);
+// ReactDOM.render(
+//     <CharacterCard name="Abigail" birthday="Fall 13"  imgUrl="/static/img/Abigail.png"/>,
+//     document.querySelector('#Abigail'),
+// );
+// ReactDOM.render(
+//     <CharacterCard name="Alex" birthday="Spring 20"  imgUrl="/static/img/Alex.png"/>,
+//     document.querySelector('#Alex'),
+// );
+// ReactDOM.render(
+//     <CharacterCard name="Elliot" birthday="Fall 05"  imgUrl="/static/img/Elliot.png"/>,
+//     document.querySelector('#Elliot'),
+// );
+// ReactDOM.render(
+//     <CharacterCard name="Emily" birthday="Spring 27"  imgUrl="/static/img/Emily.png"/>,
+//     document.querySelector('#Emily'),
+// );
+// ReactDOM.render(
+//     <CharacterCard name="Haley" birthday="Spring 14"  imgUrl="/static/img/Haley.png"/>,
+//     document.querySelector('#Haley'),
+// );
+// ReactDOM.render(
+//     <CharacterCard name="Harvey" birthday="Spring 14"  imgUrl="/static/img/Harvey.png"/>,
+//     document.querySelector('#Harvey'),
+// );
+// ReactDOM.render(
+//     <CharacterCard name="Leah" birthday="Winter 23"  imgUrl="/static/img/Leah.png"/>,
+//     document.querySelector('#Leah'),
+// );
+// ReactDOM.render(
+//     <CharacterCard name="Maru" birthday="Summer 10"  imgUrl="/static/img/Maru.png"/>,
+//     document.querySelector('#Maru'),
+// );
+// ReactDOM.render(
+//     <CharacterCard name="Penny" birthday="Fall 02"  imgUrl="/static/img/Penny.png"/>,
+//     document.querySelector('#Penny'),
+// );
+// ReactDOM.render(
+//     <CharacterCard name="Sam" birthday="Summer 17"  imgUrl="/static/img/Sam.png"/>,
+//     document.querySelector('#Sam'),
+// );
+// ReactDOM.render(
+//     <CharacterCard name="Sebastian" birthday="Winter 10"  imgUrl="/static/img/Sebastian.png"/>,
+//     document.querySelector('#Sebastian'),
+// );
+// ReactDOM.render(
+//     <CharacterCard name="Shane" birthday="Spring 20"  imgUrl="/static/img/Shane.png"/>,
+//     document.querySelector('#Shane'),
+// );
